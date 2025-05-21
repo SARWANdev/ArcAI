056ee42a3573c8386adb116fe5b1acf34d0136b7
uktup <uktup@student.kit.edu>
2025-05-21 19:52:00 +0200

    first commit structure

---
+++ MODEL/User.py
@@ -0,0 +1,12 @@
+
+class User:
+    def __init__(self, useer_id, first_name, last_name, email):
+        self.useer_id = useer_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+
+    def log_in(self):
+        pass
+    def log_out(self):
+        pass
