671f34565d40c58b84cf7599bbe501ca422914fd
uktup <uktup@student.kit.edu>
2025-05-22 15:05:05 +0200

    library structure

---
+++ MODEL/Library.py
@@ -0,0 +1,33 @@
+class Library:
+    def __init__(self):
+        pass
+
+    def create_project(self):
+        pass
+
+    def delete_project(self):
+        pass
+
+    def move(self):
+        #here it has to be a generic method that can move documents , folders and projects.
+        pass
+
+    def duplicate(self):
+        #here it has to be a generic method that can duplicate  documents , folders and projects.
+        pass
+
+    def download(self):
+        # here it has to be a generic method that can download the content   documents
+        pass
+
+    def return_embedding(self):
+        #this method should return all its conten as emding
+        pass
+
+    def search_documents(self):
+        pass
+
+    def sort_library(self):
+        pass
+
+
