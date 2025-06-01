8a24d291c2fd7ff5c7d617899c773920aa44dd78
„Chinmay <Chinmay281803@gmail.com>
2025-06-01 15:05:09 +0200

    Added Upload Button

---
+++ VIEW/src/Pages/Components/Buttons/UploadButton.jsx
@@ -0,0 +1,8 @@
+import "./UploadButton.css"
+export default function UploadButton() {
+    return (
+        <div className="upload-button-container">
+            <button className="upload-button" title="Upload">Upload</button>
+        </div>
+    )
+}
--- VIEW/src/Pages/Components/PopUpBoxProjectOptions.jsx
+++ VIEW/src/Pages/Components/PopUpBoxProjectOptions.jsx
@@ -1,11 +1,13 @@
 import "./PopUpBoxProjectOptions.css"
 import DeleteButton from "./Buttons/DeleteButton"
 import DownloadButton from "./Buttons/DownloadButton"
+import UploadButton from "./Buttons/UploadButton"
 export default function PopUpBoxProjectOptions() {
     return (
         <div className="pop-up-box-project-options" id = "project-options-container">
             <DeleteButton/>
             <DownloadButton/>
+            <UploadButton/>
         </div>
     )
 }
