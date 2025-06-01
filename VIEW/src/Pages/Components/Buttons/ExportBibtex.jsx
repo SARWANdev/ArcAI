c7ac9a66b8e4fcd82a129e27e3799f6692132003
„Chinmay <Chinmay281803@gmail.com>
2025-06-01 15:32:13 +0200

    Added export bibtex button

---
+++ VIEW/src/Pages/Components/Buttons/ExportBibtex.jsx
@@ -0,0 +1,8 @@
+import "./ExportBibtex.css"
+export default function ExportBibtex() {
+    return (
+        <div className="export-bibtex-container">
+            <button className="export-bibtex-button" title="Export Bibtex">Export Bibtex</button>
+        </div>
+    )
+}
--- VIEW/src/Pages/Components/PopUpBoxDocumentsOptions.jsx
+++ VIEW/src/Pages/Components/PopUpBoxDocumentsOptions.jsx
@@ -1,12 +1,14 @@
 import DeleteButton from "./Buttons/DeleteButton"
 import RenameButton from "./Buttons/RenameButton"
 import DuplicateButton from "./Buttons/DuplicateButton"
+import ExportBibtex from "./Buttons/ExportBibtex"
 import "./PopUpBoxDocumentsOptions.css"
 export default function PopUpBoxDocumentsOptions() {
     return (
         <div className="pop-up-box-documents-options" id = "documents-options-container">
             <DeleteButton/>
             <DuplicateButton/>
+            <ExportBibtex/>
             <RenameButton/>
         </div>
     )
