9ea57f27d6b84de7875e0133fb79b2dd5ca4ab12
„Chinmay <Chinmay281803@gmail.com>
2025-06-01 15:11:34 +0200

    Added Rename Button

---
+++ VIEW/src/Pages/Components/Buttons/RenameButton.jsx
@@ -0,0 +1,8 @@
+import "./RenameButton.css"
+export default function RenameButton() {
+    return (
+        <div className="rename-button-container">
+            <button className="rename-button" title="Rename">Rename</button>
+        </div>
+    )
+}
--- VIEW/src/Pages/Components/PopUpBoxProjectOptions.jsx
+++ VIEW/src/Pages/Components/PopUpBoxProjectOptions.jsx
@@ -2,12 +2,14 @@ import "./PopUpBoxProjectOptions.css"
 import DeleteButton from "./Buttons/DeleteButton"
 import DownloadButton from "./Buttons/DownloadButton"
 import UploadButton from "./Buttons/UploadButton"
+import RenameButton from "./Buttons/RenameButton"
 export default function PopUpBoxProjectOptions() {
     return (
         <div className="pop-up-box-project-options" id = "project-options-container">
             <DeleteButton/>
             <DownloadButton/>
             <UploadButton/>
+            <RenameButton/>
         </div>
     )
 }
