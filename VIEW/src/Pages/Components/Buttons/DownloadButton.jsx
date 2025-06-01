432a4be4b40cf4ab03da26fe40c66620fbaada54
„Chinmay <Chinmay281803@gmail.com>
2025-06-01 15:02:21 +0200

    Added Download Button

---
+++ VIEW/src/Pages/Components/Buttons/DownloadButton.jsx
@@ -0,0 +1,8 @@
+import "./DownloadButton.css"
+export default function DownloadButton() {
+    return (
+        <div className="download-button-container">
+            <button className="download-button" title="Download">Download</button>
+        </div>
+    )
+}
--- VIEW/src/Pages/Components/PopUpBoxProjectOptions.jsx
+++ VIEW/src/Pages/Components/PopUpBoxProjectOptions.jsx
@@ -1,9 +1,11 @@
 import "./PopUpBoxProjectOptions.css"
 import DeleteButton from "./Buttons/DeleteButton"
+import DownloadButton from "./Buttons/DownloadButton"
 export default function PopUpBoxProjectOptions() {
     return (
         <div className="pop-up-box-project-options" id = "project-options-container">
             <DeleteButton/>
+            <DownloadButton/>
         </div>
     )
 }
