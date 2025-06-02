e1f9721b2ad10e36a410abf6ccbe63265c52d265
„Chinmay <Chinmay281803@gmail.com>
2025-06-02 18:18:36 +0200

    Added clear all button

---
+++ VIEW/src/Pages/Components/Buttons/ClearAllButton.jsx
@@ -0,0 +1,8 @@
+import "./ClearAllButton.css"
+export default function ClearAllButton(){
+    return(
+        <button className="clear-all-button">
+            Clear All
+        </button>
+    )
+}
