8cc7c8c9ab619f38130340f7925f68d83ac4c3cf
„Chinmay <Chinmay281803@gmail.com>
2025-06-02 18:14:11 +0200

    Added Button to upload a file on chat page

--- VIEW/src/Pages/Components/AIChatContainer.jsx
+++ VIEW/src/Pages/Components/AIChatContainer.jsx
@@ -3,6 +3,7 @@ export default function AIChatContainer(){
     return(
         <form className="ai-chat-container">
             <input type="text" placeholder="Ask me anything..." className="ai-chat-container-input"/>
+            <input type="file" className="ai-chat-container-file-input"/>
         </form>
     )
 }
