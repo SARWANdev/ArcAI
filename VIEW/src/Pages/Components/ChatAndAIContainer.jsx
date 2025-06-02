3eae05ca1027df882d580f9dedffb241992796b4
„Chinmay <Chinmay281803@gmail.com>
2025-06-02 11:12:51 +0200

    Refactored names

--- VIEW/src/Pages/Components/ChatAndAIContainer.jsx
+++
@@ -1,8 +0,0 @@
-import "./ChatAndAIContainer.css"
-export default function ChatAndAIContainer(){
-    return(
-        <div className="chat-and-ai-container">
-            <h1>ChatAndAIContainer</h1>
-        </div>
-    )
-}
---
+++ VIEW/src/Pages/Components/NoteAndAIChatContainer.jsx
@@ -0,0 +1,8 @@
+import "./NoteAndAIChatContainer.css"
+export default function NoteAndAIChatContainer(){
+    return(
+        <div className="note-and-ai-chat-container">
+            <h1>NoteAndAIChatContainer</h1>
+        </div>
+    )
+}
