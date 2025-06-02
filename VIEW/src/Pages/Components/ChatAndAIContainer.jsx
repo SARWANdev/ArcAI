9b27ccab5bd179b9805c27cd0722fa08ddb86ca9
„Chinmay <Chinmay281803@gmail.com>
2025-06-02 11:02:57 +0200

    Added a Container which will contain Chat and AI chat container

---
+++ VIEW/src/Pages/Components/ChatAndAIContainer.jsx
@@ -0,0 +1,8 @@
+import "./ChatAndAIContainer.css"
+export default function ChatAndAIContainer(){
+    return(
+        <div className="chat-and-ai-container">
+            <h1>ChatAndAIContainer</h1>
+        </div>
+    )
+}
