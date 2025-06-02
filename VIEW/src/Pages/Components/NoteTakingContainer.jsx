02f724b46367cad157e14be469bb2a8d84df3aca
„Chinmay <Chinmay281803@gmail.com>
2025-06-02 10:58:41 +0200

    Added Note taking container

---
+++ VIEW/src/Pages/Components/NoteTakingContainer.jsx
@@ -0,0 +1,8 @@
+import "./NoteTakingContainer.css"
+export default function NoteTakingContainer(){
+    return(
+        <div className="note-taking-container">
+            <h1>Note Taking Container</h1>
+        </div>
+    )
+}
