b6b3fa73be126b3a30ab9315b4ac34e02903ecda
„Chinmay <Chinmay281803@gmail.com>
2025-06-02 11:17:06 +0200

    Added AI Chat container

---
+++ VIEW/src/Pages/Components/AIChatContainer.jsx
@@ -0,0 +1,8 @@
+import "./AIChatContainer.css"
+export default function AIChatContainer(){
+    return(
+        <div className="ai-chat-container">
+            <h1>AIChatContainer</h1>
+        </div>
+    )
+}
