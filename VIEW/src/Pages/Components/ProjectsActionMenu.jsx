910ce1102cdf37c4b8fe9377d34bd0f6118c4d96
„Chinmay <Chinmay281803@gmail.com>
2025-06-09 12:37:06 +0200

    Changed name from PopUpBoxProjectOptions to ProjectsActionMenu

--- VIEW/src/Pages/Components/ProjectsActionMenu.jsx
+++ VIEW/src/Pages/Components/ProjectsActionMenu.jsx
@@ -1,4 +1,4 @@
-import "./PopUpBoxProjectOptions.css"
+import "./ProjectsActionMenu.css"
 import DeleteButton from "./Buttons/DeleteButton"
 import DownloadButton from "./Buttons/DownloadButton"
 import UploadButton from "./Buttons/UploadButton"
@@ -7,7 +7,7 @@ import RenameButton from "./Buttons/RenameButton"
  * PopUpBoxProjectOptions is the component that is used to display the pop up box for the project options.
  * @returns {JSX} - The React component for the pop up box for the project options.
  */
-export default function PopUpBoxProjectOptions() {
+export default function ProjectsActionMenu() {
     return (
         <div className="pop-up-box-project-options" id = "project-options-container">
             <DeleteButton/>
