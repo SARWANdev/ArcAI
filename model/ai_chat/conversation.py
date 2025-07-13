3936918370ba7b83314320eb1ba0914a6fa3e603
Eren Sönmez <erensonmez03@gmail.com>
2025-07-13 16:48:40 +0200

    feat: enhance Conversation class with created_at and updated_at attributes; add to_dict method for serialization

--- model/ai_chat/conversation.py
+++ model/ai_chat/conversation.py
@@ -1,7 +1,10 @@
 from langchain_community.vectorstores import FAISS
+from typing import Dict, Any
+from datetime import datetime
+
 class Conversation:
     def __init__(self, conversation_id=None, user_id=None, name=None, list_of_documents=None,
-                  human_messages=None, ai_messages=None, vector_store=None):
+                  human_messages=None, ai_messages=None, vector_store=None, created_at=None, updated_at=None):
         self.conversation_id = conversation_id
         self.user_id = user_id
         self.name = name
@@ -9,6 +12,8 @@ class Conversation:
         self.human_messages = human_messages or []
         self.ai_messages = ai_messages or []
         self.vector_store = vector_store
+        self.created_at = created_at
+        self.updated_at = updated_at
 
     def rename(self, name):
         self.name = name
@@ -18,6 +23,19 @@ class Conversation:
 
     def add_ai_message(self, msg: str):
         self.ai_messages.append(msg)
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            "_id": self.conversation_id,
+            "user_id": self.user_id,
+            "name": self.name,
+            "list_of_documents": self.list_of_documents,
+            "human_messages": self.human_messages,
+            "ai_messages": self.ai_messages,
+            "vector_store": self.vector_store,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at or datetime.utcnow(),
+        }
         
     def __format_user_message(self, message:str, context: str)->str:
         formatted_message = f"""  
