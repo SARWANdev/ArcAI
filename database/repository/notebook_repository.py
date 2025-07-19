fedd6f72c17ed2c5151206a8580733145ce25e0a
uktup <uktup@student.kit.edu>
2025-07-19 17:02:14 +0200

    database repositories refactor

--- database/repository/notebook_repository.py
+++ database/repository/notebook_repository.py
@@ -1,18 +1,21 @@
+from bson import ObjectId
+
 from database.utils.mongo_connector import mongo_connection
 
 class Notebook:
 
+
     @staticmethod
     def get_project_notebook(project_id) -> str:
         with mongo_connection() as db:
-            return db.projects.find_one({"_id": project_id})["note"]
+            return db.projects.find_one({"_id": ObjectId(project_id)})["note"]
 
 
     @staticmethod
     def update_project_notebook(project_id, note) -> bool:
         try:
             with mongo_connection() as db:
-                result = db.projects.update_one({"_id": project_id}, {"$set": {"note": note}})
+                result = db.projects.update_one({"_id": ObjectId(project_id)}, {"$set": {"note": note}})
                 return result.modified_count > 0
         except Exception as e:
             print(f"Project notebook could not be update: {e}")
@@ -22,19 +25,17 @@ class Notebook:
     @staticmethod
     def get_document_notebook(document_id) -> str:
         with mongo_connection() as db:
-            return db.documents.find_one({"_id": document_id})["note"]
+            return db.documents.find_one({"_id": ObjectId(document_id)})["note"]
 
 
     @staticmethod
     def update_document_notebook(document_id, note) -> bool:
         try:
             with mongo_connection() as db:
-                result = db.documents.update_one({"_id": document_id}, {"$set": {"note": note}})
+                result = db.documents.update_one({"_id": ObjectId(document_id)}, {"$set": {"note": note}})
                 return result.modified_count > 0
         except Exception as e:
             print(f"Document notebook could not be update: {e}")
             return False
 
 
-        
-    
