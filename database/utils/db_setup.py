b31207061a673f624896c3f697b0cddad4dfdc18
DanielCamVi <ukwyq@student.kit.edu>
2025-07-19 15:59:28 +0200

    Update Elasticsearch connection in db_setup.py and remove unnecessary whitespace in db_launcher.py

--- database/utils/db_setup.py
+++ database/utils/db_setup.py
@@ -10,4 +10,9 @@ projects = db["projects"]
 documents = db["documents"]
 conversations = db["conversations"]
 
-es = Elasticsearch("http://localhost:9200", basic_auth=("elastic", "TB=IE8CncAGH6+2jG48w")) #JUST FOR DANI
+#es = Elasticsearch("http://localhost:9200", basic_auth=("elastic", "TB=IE8CncAGH6+2jG48w")) #JUST FOR DANI
+es = Elasticsearch(
+    "https://127.0.0.1:9200",
+    basic_auth=("elastic", "ZJM0fN6SIt=Zm0=JQZ5H"),
+    verify_certs=False  # Ignore certified autosigned
+)
