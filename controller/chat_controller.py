0ac751f40d8c11982e193b32cce8fddb9f334cea
„Chinmay <Chinmay281803@gmail.com>
2025-07-22 15:11:54 +0200

    refactoring controller

--- controller/chat_controller.py
+++ controller/chat_controller.py
@@ -10,8 +10,9 @@ from database.repository.conversation_repository import ConversationRepository
 from pathlib import Path
 import os
 
+
 class ChatController:
-    def __init__(self, app : Flask):
+    def __init__(self, app: Flask):
         self.ai_service = AIService()
         self.chat = Blueprint('chat', __name__)
         self.document_service = DocumentService()
@@ -28,7 +29,6 @@ class ChatController:
         # 7th step : Use ai_service.output_streaming_response method to stream the response
         # 8th step : The string output wil be added to Conversation model (add_ai_message)
 
-
         # 1st step
         self.register_chat_routes(app)
 
@@ -43,19 +43,30 @@ class ChatController:
 
             if not user_id:
                 return jsonify({"error": "user_id is required"}), 400
-
+            print(1)
             new_conversation = self.conversation_service.create_conversation(user_id, document_ids, project_ids)
+            print(2)
             new_conversation.add_user_message(user_prompt)
+            print(3)
+            new_conversation.set_name(self.ai_service.generate_conversation_name(new_conversation))
+            print(4)
             response = self.ai_service.send_chat_message(question=user_prompt, conversation=new_conversation)
+            print(5)
             string_response = self.ai_service.output_streaming_response(response, len)
+            print(6)
             new_conversation.add_ai_message(string_response)
+            print(7)
             self.conversation_service.update_messages(new_conversation.conversation_id, new_conversation.get_messages())
+            print(9)
             new_conversation.get_messages().pop(0)
             list_of_messages = new_conversation.get_messages()
+            print(10)
+            print(new_conversation.conversation_id)
+            print(new_conversation.name)
             return jsonify({
                 "success": True,
-                "data": {"conversation_id" : new_conversation.conversation_id,
-                         "list_of_messages" : list_of_messages}
+                "data": {"conversation_id": str(new_conversation.conversation_id),
+                         "list_of_messages": list_of_messages}
             }), 200
 
         except Exception as e:
@@ -70,23 +81,32 @@ class ChatController:
             data = request.get_json()
             user_id = data.get("user_id")
             user_prompt = data.get("user_prompt")
-            document_ids = data.get("document_ids")
-            project_ids = data.get("project_ids")
+            conversation_id = data.get("conversation_id")
 
             if not user_id:
                 return jsonify({"error": "user_id is required"}), 400
 
-            print(user_id, user_prompt, document_ids, project_ids)
-
+            old_conversation = self.conversation_service.get_conversation(conversation_id)
+            old_conversation.add_user_message(user_prompt)
+            response = self.ai_service.send_chat_message(question=user_prompt, conversation=old_conversation)
+            print(21)
+            string_response = self.ai_service.output_streaming_response(response, len)
+            print(22)
+            old_conversation.add_ai_message(string_response)
+            print(23)
+            self.conversation_service.update_messages(old_conversation.conversation_id, old_conversation.get_messages())
+            print(24)
+            list_of_messages = old_conversation.get_messages()
+            print(list_of_messages)
             return jsonify({
                 "success": True,
-                "data": "The follow up has been a success"
+                "data": {"conversation_id": str(old_conversation.conversation_id),
+                         "list_of_messages": list_of_messages}
             }), 200
 
         except Exception as e:
             print(e)
             return jsonify({"error": str(e)}), 500
-
 
     def get_chat_history(self, user_id):
         pass
@@ -105,7 +125,7 @@ class ChatController:
         Deletes a chat session and its messages.
         """
         return self.ai_service.delete_chat(chat_id)
-    
+
     def delete_all_chats(self, user_id):
         """
         Deletes all chat sessions for a user.
