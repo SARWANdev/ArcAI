0391df26493e70f47aa91b2cdfc911bf3c8b28e5
„Chinmay <Chinmay281803@gmail.com>
2025-07-23 22:26:54 +0200

    get searches

--- controller/library_controller.py
+++ controller/library_controller.py
@@ -179,6 +179,33 @@ class LibraryController:
     def get_item_metadata(self, item_id, item_type):
         pass
 
+    def search_documents(self):
+        try:
+            # Get parameters from query string
+            user_id = request.args.get("user_id")
+            query = request.args.get("query")
+            print(user_id, query)
+            print
+
+            if not user_id:
+                return jsonify({"error": "user_id is required"}), 400
+
+            print("searching")
+            searches = self.document_service.search_documents(user_id, query)
+            documents = []
+            for document in searches:
+                dictionary = {"Title" : document.name, "DocumentId" : document.document_id}
+                documents.append(dictionary)
+            print(documents)
+
+            return jsonify({
+                "status": "success",
+                "results": documents,
+                "message": "Search retrieved successfully",
+            }), 200
+        except Exception as e:
+            return jsonify({"status": "error", "message":str(e)}),500
+
     # To register all the library-routes
     def register_library_routes(self, app):
         self.library.add_url_rule("/library/create-project", view_func=self.create_project, methods=["POST"])
@@ -186,4 +213,5 @@ class LibraryController:
         self.library.add_url_rule("/library/get-projects", view_func=self.get_user_projects)
         self.library.add_url_rule("/library/delete-project", view_func=self.delete_project, methods=["DELETE"])
         self.library.add_url_rule("/library/rename-project", view_func=self.rename_project, methods=["PATCH"])
+        self.library.add_url_rule("/library/search", view_func=self.search_documents)
         app.register_blueprint(self.library)
