92ffbf24a6565e78d210c8dc632a7a8966854d5c
DanielCamVi <ukwyq@student.kit.edu>
2025-07-24 15:21:51 +0200

    burned history to the ground

--- model/ai_chat/history.py
+++
@@ -1,9 +0,0 @@
-class History:
-    def __init__(self, conversations):
-        self.conversations = []
-
-    def add_conversation(self, conversation):
-        self.conversations.append(conversation)
-    
-    def delete_conversation(self, conversation):
-        self.conversations.remove(conversation)
