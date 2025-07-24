329cf58f7343adc9f1e7e317088986b07a031e0f
uktup <uktup@student.kit.edu>
2025-07-24 15:36:53 +0200

    user model and repo refactor

--- database/repository/user_repository.py
+++ database/repository/user_repository.py
@@ -6,6 +6,7 @@ from database.repository.date_time_utils import get_utc_zulu_timestamp
 from database.utils.mongo_connector import mongo_connection
 from typing import List, Dict
 
+from model.user_profile.user import User
 
 
 class User:
@@ -20,7 +21,6 @@ class User:
         self.updated_at = self.created_at
 
     def new_user(self):
-        #test line
         if not self.sub_id:
             raise ValueError("Google 'sub' ID is required")
 
@@ -44,71 +44,25 @@ class User:
                 return str()
 
     @staticmethod
-    def get_all_users() -> List[Dict]:
-        with mongo_connection() as db:
-            users = list(db.users.find({}, {'_id': 1, 'first_name': 1, 'last_name': 1,
-                                            'email': 1}))
-            #take care what fields are included with the boolean variables. TO CHECK
-            return users
-
+    def save(user: User) -> str:
+        with mongo_connection() as database_connection:
+            result = database_connection.users.insert_one( user.new_user_dict() )
+            user_id = str(result.inserted_id)
+            return user_id
     @staticmethod
     def get_user_by_id(user_id: str) -> dict:
         try:
             with mongo_connection() as db:
-                return db.users.find_one({"_id": ObjectId(user_id) })
+                return db.users.find_one({"_id": user_id })
         except Exception as e:
             print(f"Database error: {str(e)}")
             raise
-
-    @staticmethod
-    def get_user_by_email(email: str) -> dict:
-        if not email or "@" not in email:
-            raise ValueError("Invalid email format")
-
-        try:
-            with mongo_connection() as db:
-                return db.users.find_one({"email": email})
-        except Exception as e:
-            print(f"Database error: {str(e)}")
-            raise
-
-    @staticmethod
-    def update_user_name(user_id: str, new_name: str) -> bool:
-        try:
-            with mongo_connection() as db:
-                result = db.users.update_one(
-                    {'_id': ObjectId(user_id)},
-                    {'$set': {
-                        'first_name': new_name,
-                        "updated_at": get_utc_zulu_timestamp(),
-                    }}
-                )
-                return result.modified_count > 0
-        except Exception as e:
-            print(f"Error updating user: {str(e)}")
-            return False
-
-    @staticmethod
-    def update_user_last_name(user_id: str, new_last_name: str) -> bool:
-        try:
-            with mongo_connection() as db:
-                result = db.users.update_one(
-                    {'_id': ObjectId(user_id)},
-                    {'$set': {
-                        'last_name': new_last_name,
-                        "updated_at": get_utc_zulu_timestamp(),
-                    }}
-                )
-                return result.modified_count > 0
-        except Exception as e:
-            print(f"Error updating user: {str(e)}")
-            return False
 
     @staticmethod
     def update_view_mode(user_id: str, view_mode: bool) -> bool:
         try:
             with mongo_connection() as db:
-                result = db.users.update_one({"_id": ObjectId(user_id)},
+                result = db.users.update_one({"_id": user_id},
                                              {"$set": {"view_mode": view_mode, "updated_at": get_utc_zulu_timestamp()}})
                 return result.modified_count > 0
 
@@ -120,8 +74,19 @@ class User:
     def deactivate_user(user_id: str):
         try:
             with mongo_connection() as db:
-                result = db.users.update_one({"_id": ObjectId(user_id)},
+                result = db.users.update_one({"_id": user_id},
                                              {"$set": {"active": False, "updated_at": get_utc_zulu_timestamp()}})
+                return result.modified_count > 0
+        except Exception as e:
+            print(f"Error deactivating the account: {str(e)}")
+            return False
+
+    @staticmethod
+    def activate_user(user_id: str):
+        try:
+            with mongo_connection() as db:
+                result = db.users.update_one({"_id": user_id},
+                                             {"$set": {"active": True, "updated_at": get_utc_zulu_timestamp()}})
                 return result.modified_count > 0
         except Exception as e:
             print(f"Error deactivating the account: {str(e)}")
--- model/user_profile/user.py
+++ model/user_profile/user.py
@@ -1,3 +1,4 @@
+from database.repository.date_time_utils import get_utc_zulu_timestamp
 from model.user_profile.view_mode import ViewMode
 
 class User:
@@ -7,3 +8,17 @@ class User:
         self.last_name = last_name
         self.email = email
         self.prefered_mode = ViewMode.LIGHT  # by default maybe it's set to light mode
+
+    def new_user_dict(self):
+        user_dic = {
+            "_id": self.user_id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "view_mode": False,
+            "active": True,
+
+            "created_at": get_utc_zulu_timestamp(),
+            "updated_at": get_utc_zulu_timestamp(),
+        }
+        return user_dic
