e597dda6fe41f075f5a9a59e556d8898b94cbf42
uktup <uktup@student.kit.edu>
2025-07-24 23:01:35 +0200

    document thread model, service, repository deletion from empty methods

--- services/document_service.py
+++ services/document_service.py
@@ -62,14 +62,11 @@ class DocumentService:
                                                  year = bibtex_instance.get_year(), source = bibtex_instance.get_source(),
                                                  authors = str(bibtex_instance.get_authors()), bibtex= bibtex_str,
                                                  first_author= bibtex_instance.get_first_author(),)
-        # TODO eather update the instance or the database described with the bibtex
 
-
-           # string type
         pdf_master_id = self.pdf_master_repository.save(new_pdf_master_instance)
         return pdf_master_id
 
-    # this method is possibly the one that has to be called
+    # Endpoint Method
     def upload_file(self, file, user_id, project_id):
         original_name = file.filename
         suffix = "." + file.filename.split(".")[1]
@@ -412,12 +409,6 @@ class DocumentService:
         pdf = self.document_repository.get_pdf(document_id, path)
         return pdf
 
-    def highlight_document(self, document_id, text):
-        pass
-
-    def process_document_metadata(self, document_id):
-        pass
-
     def duplicate_document(self, document_id, project_id):
         """
         1. gets the pdf_master_id
@@ -441,15 +432,6 @@ class DocumentService:
 
         self.document_properties_repo.set_new_project_id(document_id, new_project_id)
 
-
-
-
-    def extract_text_from_document(self, document_id):
-        pass
-
-    def get_text_chunks_from_document(self, document_id):
-        pass
-
     def download_bibtex(self, document_id):
         # Get a document's bibtex and returns it as a buffer, with it's name
         document_data = self.document_repository.get_by_document_id(document_id)
@@ -460,11 +442,6 @@ class DocumentService:
         buffer = io.BytesIO()
         buffer.write(bibtex.encode('utf-8'))
         return buffer, title
-        
-
-    def name_assigner(self):
-        #takes the pdf information from the Bibtex and assigns a name, possibly athorLastName-first3Wordsof the title and date
-        return str()
     
     def search_documents(self, user_id, query):
         hits = self.document_repository.search_documents(user_id, query)
@@ -480,10 +457,6 @@ class DocumentService:
             document_data = self.document_repository.get_by_document_id(id)
             document_list.append(DocumentModel.from_dict(document_data))
         return document_list
-
-
-    def get_document_vector_store(self, document_id):
-        pass
 
     def __rename_according_ref_count(self, document_name: str, pdf_master_id: str) -> str:
         ref_count = self.pdf_master_repository.get_ref_count(pdf_master_id)
