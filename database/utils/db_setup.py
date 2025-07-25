75edf47b71378054668d70a2ab93103c0cc7124c
DanielCamVi <ukwyq@student.kit.edu>
2025-07-25 15:10:39 +0200

    erased a comment

--- database/utils/db_setup.py
+++ database/utils/db_setup.py
@@ -13,7 +13,6 @@ projects = db["projects"]
 documents = db["documents"]
 conversations = db["conversations"]
 
-#es = Elasticsearch("http://localhost:9201", basic_auth=("elastic", "TB=IE8CncAGH6+2jG48w")) #JUST FOR DANI
 es = Elasticsearch(
     os.getenv("ELASTIC_URI", "https://127.0.0.1:9200"),
     basic_auth=(
