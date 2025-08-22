5a252bcafd7075a1d5caf5a399c418ea9396fe16
uktup <uktup@student.kit.edu>
2025-08-22 15:40:48 +0200

    deletion from error code and details for base exception

--- exceptions/base_exceptions.py
+++ exceptions/base_exceptions.py
@@ -4,8 +4,7 @@ class ArcAIException(Exception):
     def __init__(self, message: str, error_code: str = None, details: dict = None):
         super().__init__(message)
         self.message = message
-        self.error_code = error_code
-        self.details = details or {}
+
 
 class ValidationException(ArcAIException):
     """Base class for validation-related errors"""
